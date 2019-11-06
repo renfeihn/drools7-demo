0e8c10e1ce436d7ed3c5206460c74531ef9d1917
renfei <renfei_hn@163.com>
2019-11-06 17:41:12 +0800

    init

---
+++ src/test/java/com/sample/RuleFlow.java
@@ -0,0 +1,56 @@
+package com.sample;
+
+public class RuleFlow {
+	/**
+	 * 名称
+	 */
+	private String userName;
+
+	/**
+	 * 性别
+	 */
+	private String sex;
+
+	/**
+	 * 年龄
+	 */
+	private int age;
+
+	/**
+	 * 成绩
+	 */
+	private double source;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getSource() {
+		return source;
+	}
+
+	public void setSource(double source) {
+		this.source = source;
+	}
+
+}
